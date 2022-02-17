7aa4f26b02b6d4f096d13364c38c994ceca98c9f
Edson Bastos <edsonbastos2@hotmail.com>
2022-02-16 22:49:49 -0300

    Feito get de transações e listando na página de transações

--- store/transactions/actions.js
+++ store/transactions/actions.js
@@ -1,4 +1,9 @@
 export default {
+
+  getTransactions(){
+    return this.$axios.$get('transactions?_expand=category')
+  },
+
   addTransactions(context, data){
     return this.$axios.$post('transactions', data)
   }
